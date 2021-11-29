936927f9a21493fdf1043ab0c12c6a6594ebaf8d
sarasabet <sara.ilbeigi@gmail.com>
2021-11-29 16:43:52 -0500

    add login.get auth to models/auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -24,5 +24,20 @@ router.post("/register", async (req, res) => {
     res.status(500).json(err);
   }
 });
+// login
+router.post('/login', async(req, res)=> {
+  try{
+    const user = await User.findOne({username:req.body.username})
+    !user && res.status(400).json('Wrong credentials')
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json('Wrong credentials')
+// to take out password , an dsend just the necessary detail
+    const {password, ...others} = user._doc
+    res.status(200).json(others);
+  }catch(err) {
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router;
